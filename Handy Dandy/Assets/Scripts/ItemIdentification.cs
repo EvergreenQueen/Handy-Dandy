1b259342c059547d4e767da2e5192f1fe0d8c149
Ruth Sun <rsun050@ucr.edu>
2024-05-26 21:33:00 -0700

    add item hover ui

--- Handy Dandy/Assets/Scripts/ItemIdentification.cs
+++ Handy Dandy/Assets/Scripts/ItemIdentification.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class ItemIdentification : MonoBehaviour
 {
     public enum ListOfPossibleTags{Hot, Cold, Animal, Fruit, Child, Grippable, Big}
-    [SerializeField] string name;
+    [SerializeField] public string name;
     [SerializeField] public int id;
-    [SerializeField] ListOfPossibleTags[] tags;
-    [SerializeField] string description;
+    [SerializeField] public ListOfPossibleTags[] tags;
+    [SerializeField] public string description;
 
     public bool containsTag(ListOfPossibleTags t)
     {
